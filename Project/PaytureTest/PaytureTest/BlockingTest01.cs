4fb636eb0eba5033932113fd8d677d9ca2f50e0d
AntonRassadin <antonrsdn@gmail.com>
2019-04-25 22:46:57 +0300

    Add Project

---
+++ Project/PaytureTest/PaytureTest/BlockingTest01.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaytureTest
+{
+    /*
+    [TestClass]
+    public class TetsInTest
+    {
+        private const string URL = "https://sandbox3.payture.com/api/Block";
+        [DataTestMethod]
+        [DataRow(1755, "Merchant", 5218851946955484, 12, 21, "Ivan Ivanov", 123)]
+        public void Test(int amount, string key, long PAN, int eMonth, int eYear, string cardHolder, int secureCode)
+        {
+            string orderId = Guid.NewGuid().ToString("N");
+            Console.WriteLine(orderId);
+            var builder = new UriBuilder(URL);
+            builder.Port = -1;
+            var query = HttpUtility.ParseQueryString(builder.Query);
+            query["Key"] = key;
+            query["OrderId"] = orderId;
+            query["Amount"] = amount.ToString();
+            string payInfo = $"PAN={PAN}; EMonth={eMonth}; EYear={eYear}; CardHolder={cardHolder}; " +
+                $"SecureCode={secureCode}; OrderId = {orderId}; Amount = {amount};";
+            query["PayInfo"] = payInfo;
+            builder.Query = query.ToString();
+            string url = builder.ToString();
+                        
+            Console.WriteLine(url);
+        }
+    }
+    */
+    [TestClass]
+    public class PaytureApiTest
+    {
+        private const string URL = "https://sandbox3.payture.com/api/Block";
+        [DataTestMethod]
+        [DataRow(1050, "Merchant", 5218851946955484, 12, 21, "Ivan Ivanov", 123, true)]
+        [DataRow(1755, "Merchant", 3300000000000001, 12, 22, "Ivan Ivanov", 521, false)]
+        [DataRow(2184, "Merchant", 5218851946955484, 12, 21, "Ivan Ivanov", 123, true)]
+
+        public void BlockTest(int amount, string key, long PAN, int eMonth, int eYear, string cardHolder, int secureCode, bool successStatus)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //building uri
+            string orderId = Guid.NewGuid().ToString("N");//generate Globally Unique Identifier
+            var builder = new UriBuilder(URL);
+            builder.Port = -1;
+            var query = HttpUtility.ParseQueryString(builder.Query);
+            query["Key"] = key;
+            query["OrderId"] = orderId;
+            query["Amount"] = amount.ToString();
+            string payInfo = $"PAN={PAN}; EMonth={eMonth}; EYear={eYear}; CardHolder={cardHolder}; " +
+                $"SecureCode={secureCode}; OrderId = {orderId}; Amount = {amount};";
+            query["PayInfo"] = payInfo;
+            builder.Query = query.ToString();
+            string urlParameters = builder.ToString();
+            Console.WriteLine(urlParameters);
+
+            //Sending request
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var responseText = response.Content.ReadAsStringAsync().Result;  
+                Console.WriteLine(responseText);
+                string blockSuccess;
+                
+                //Finding "Success" parameter
+                Match match = Regex.Match(responseText, "(?<=Success=\")(.*?)(?=\")");
+                if (match.Success)
+                {
+                    blockSuccess = match.Value;
+                }
+                else
+                {
+                    throw new Exception("Incorrect response");
+                }
+                                
+                Assert.IsTrue(blockSuccess == successStatus.ToString());
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            client.Dispose();
+        }
+    }
+}
---
+++ Project/PaytureTest/PaytureTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PaytureTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PaytureTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83af1b5f-880d-42ce-aba1-3d172d1cf98e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
